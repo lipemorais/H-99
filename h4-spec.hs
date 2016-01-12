0b366acd778758c1a7279c75d240c621ffae4235
Felipe de Morais <fmorais@thoughtworks.com>
2016-01-11 23:06:00 -0200

    Give two solutions for h4 problem

---
+++ h4-spec.hs
@@ -0,0 +1,13 @@
+import Test.Hspec
+import H4
+
+
+main :: IO ()
+
+main = hspec $ do
+    describe "Find the length of a list" $ do
+        it "valid the size of a empty list is 0" $ do
+            myLength [] `shouldBe` 0
+
+        it "valids the length of [1..3] is 3" $ do
+            myLength [1..3] `shouldBe` 3
---
+++ h4.hs
@@ -0,0 +1,18 @@
+-- 4 Problem 4
+-- (*) Find the number of elements of a list.
+--
+-- Example in Haskell:
+--
+-- Prelude> myLength [123, 456, 789]
+-- 3
+-- Prelude> myLength "Hello, world!"
+-- 13
+
+module H4 where
+
+myLength :: [a] -> Int
+myLength [] = 0
+myLength (_:xs) = 1 + myLength xs
+
+myLength' :: [a] -> Int
+myLength' xs = foldr (\_ n -> 1 + n) 0 xs
