d251c5bee954cf233a796063085e9f265138913a
Felipe de Morais <fmorais@thoughtworks.com>
2016-02-14 02:00:46 -0200

    problem 8 with tests

---
+++ h8-spec.hs
@@ -0,0 +1,18 @@
+import Test.Hspec
+import H8
+
+main :: IO ()
+
+main = hspec $ do
+    describe "compress" $ do
+        it  "compress [3,3] in [3]" $ do
+            compress [3,3] `shouldBe` [3]
+            compress' [3,3] `shouldBe` [3]
+
+        it  "compress [1..3] in [1..3]" $ do
+            compress [1..3] `shouldBe` [1..3]
+            compress' [1..3] `shouldBe` [1..3]
+
+        it  "compress aaaabccaadeeee in abcade" $ do
+            compress "aaaabccaadeeee" `shouldBe` "abcade"
+            compress' "aaaabccaadeeee" `shouldBe` "abcade"
---
+++ h8.hs
@@ -0,0 +1,27 @@
+-- 8 Problem 8
+-- (**) Eliminate consecutive duplicates of list elements.
+--
+-- If a list contains repeated elements they should be replaced with a single copy of the element. The order of the elements should not be changed.
+--
+-- Example:
+--
+-- * (compress '(a a a a b c c a a d e e e e))
+-- (A B C A D E)
+-- Example in Haskell:
+--
+-- > compress "aaaabccaadeeee"
+-- "abcade"
+
+module H8 where
+
+compress :: Eq a => [a] -> [a]
+compress (x:[]) = [x]
+compress (x:y:xs)
+    | x == y = compress (y:xs)
+    | otherwise = x : compress (y:xs)
+
+compress' :: Eq a => [a] -> [a]
+compress' (x:[]) = [x]
+compress' (x:y:xs) = if x == y
+    then compress' (y:xs)
+    else x : compress' (y:xs)
