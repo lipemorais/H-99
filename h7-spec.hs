a0f28edd2cc1f4709447984ce88967a7b5083963
Felipe de Morais <fmorais@thoughtworks.com>
2016-02-14 00:03:13 -0200

    problem 7 with tests

---
+++ h7-spec.hs
@@ -0,0 +1,15 @@
+import Test.Hspec
+import H7
+
+main :: IO ()
+
+main = hspec $ do
+    describe "flatten" $ do
+        it  "flatten (Elem 3) in [3]" $ do
+            flatten (Elem 3) `shouldBe` [3]
+        it "flatten (Elem 4, Elem 3) in [4,3]" $ do
+            flatten (List [Elem 4, Elem 3]) `shouldBe` [4, 3]
+        it "flatten (List [Elem 4, List [Elem 3, Elem 2]]) in [4,3,2]" $ do
+            flatten (List [Elem 4, List [Elem 3, Elem 2]])  `shouldBe` [4, 3, 2]
+        it "flatten (List [Elem 1, List [Elem 2, List [Elem 3, Elem 4], Elem 5]]) in [1, 2, 3, 4, 5]" $ do
+            flatten (List [Elem 1, List [Elem 2, List [Elem 3, Elem 4], Elem 5]]) `shouldBe` [1..5]
